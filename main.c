3d05f91af6d71c14dc8881a7c462b07aef979214
Thiago Evaristo <123104885+evaristoT@users.noreply.github.com>
2023-05-03 15:25:28 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(void){
+  
+  char location;
+  int totalTables = 0, totalBookings = 0;
+  RESERVA reservations;  
+  DATA openingTime, closingTime; 
+}
+
+printf("-----------------------------------------------");
+printf("Welcome to the Restaurant Reservation system:\n 01.Add Reservation\n02.Delete Reservation \n03.List marked reservation\n04. Search Reservation\n05. Edit reservation record\ n06.Check reservation for a customer\n07. Exit"
+
+
+
+
+
+
+
+
+
+
+
